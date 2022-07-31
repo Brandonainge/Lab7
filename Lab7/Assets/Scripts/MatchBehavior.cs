767456dd1aec12ff408bcfc10d752035aac4eb9f
Brandon Ainge <10888736@uvu.edu>
2022-07-30 21:31:04 -0600

    Finished Lab

---
+++ Lab7/Assets/Scripts/MatchBehavior.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MatchBehavior : IDContainerBehavior
+{
+   public UnityEvent matchEvent, noMatchEvent, noMatchDelayedEvent;
+
+   private IEnumerator OnTriggerEnter(Collider other)
+   {
+	  var tempObj = other.GetComponent<IDContainerBehavior>();
+	  if (tempObj == null)
+		  yield break;
+
+	  var otherID = tempObj.idObj;
+	  if (otherID == idObj)
+	  {
+		  matchEvent.Invoke();
+	  }
+	  else
+	  {
+		  noMatchEvent.Invoke();
+		  yield return new WaitForSeconds(0.5f);
+		  noMatchDelayedEvent.Invoke();
+	  }
+   }
+}
