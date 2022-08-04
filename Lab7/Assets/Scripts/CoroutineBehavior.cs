c987643650fe306ccef336ffddd2f28e7cc2ad35
Brandon Ainge <10888736@uvu.edu>
2022-08-03 18:38:39 -0600

    Followed the lab

---
+++ Lab7/Assets/Scripts/CoroutineBehavior.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, RepeatUntilFalseEvent;
+
+    public IntData counterNum;
+
+    public bool canRun;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    private void Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        startEvent.Invoke();
+    }
+
+   
+    public void StartCounting()
+    {
+        StartCoroutine(routine: Counting());
+    }
+
+    private IEnumerator Counting()
+    {
+        
+
+        startCountEvent.Invoke();
+        yield return wfsObj;
+        while (counterNum.value > 0)
+        {
+            repeatCountEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+        }
+        endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(routine: RepeatUntilFalse());
+    }
+
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (canRun)
+        {
+            yield return wfsObj;
+            RepeatUntilFalseEvent.Invoke();
+        }
+    }
+}
